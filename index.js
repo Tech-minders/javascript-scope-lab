93cbd928fd80ce9d97e57304f234554a16f7b563
Tech-minders <lucynjoka2025@gmail.com>
2025-12-19 01:15:08 +0300

    edited index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+
+// burgers are in an array with elements
+const burgers = ['Hamburger', 'Cheeseburger'];
+
+let featuredDrink = 'Strawberry Milkshake';
+
+// if statement: condition must be true
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+}
+
+// addBurger
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+// changeFeaturedDrink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
